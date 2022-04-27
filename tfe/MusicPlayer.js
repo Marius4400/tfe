a173866cc47a2dfe53d69ca45ac1870c3f148808
marius.delvigne <marius.delvigne@gmail.com>
2022-04-27 11:00:11 +0200

    add music avec serveur

--- tfe/MusicPlayer.js
+++ tfe/MusicPlayer.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { StyleSheet, TouchableOpacity, View, Image, Text } from 'react-native'
+import { 
+    StyleSheet, 
+    TouchableOpacity, 
+    View, 
+    Image, 
+    Text } from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import { Audio } from 'expo-av'
  
